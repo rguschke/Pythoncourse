ca16757ce5b84598d352c366546201eb6c768efd
Raphi <raphi@localadmins-MacBook-Pro.local>
2018-08-22 09:08:26 +0200

    firstcommit

---
+++ firststeps.py
@@ -0,0 +1,60 @@
+#calculate with python
+
+print "I will count my chicken"
+print "hens" , 25 + 30 / 6
+print "Rossters" , 100 - 25 * 3 % 4
+print "Now i will count eggs"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "Is it true that 3 +2 < 5 - 7"
+print 3 + 2 < 5 - 7
+print "What is 3 + 2?" , 3 + 2
+print "What is 5 - 7?" , 5 - 7
+print "Oh, that's why it's false."
+print "How about some more?"
+print "is it greater?" , 5 > -2 
+print "is it greater or equal?" ,  5 >= -2
+print "Is it less or equal?" , 5 <= -2
+print 7.0 / 4.0
+print 7 / 4
+
+
+#Variables and Names
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print "there are" , cars , "cars available."
+print "There are only" , drivers , "drivers available."
+print "There will be" , cars_not_driven , "emptey cars today"
+print "We cant transport" , carpool_capacity , "people today"
+print "We have" , passengers , "to carpool today"
+print "we need to put" , average_passengers_per_car , "in each car"
+
+
+#morevariables and Printing
+
+my_name = 'raphi'
+my_age = 34 #yes
+my_height = '174 #cm' 
+my_weight = 72
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "Let's talk about %s." % my_name 
+print "He is %d cm tall." % my_height
+print "he is %d heavy" % my_weight
+print "Not too heavy"
+print "He's got %s hair and %s eyes" % (my_hair, my_eyes)
+print "If i add %d, %d, and %d i get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+
+
+
+
