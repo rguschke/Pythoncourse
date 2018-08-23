d801a00c6f9c63dc32ca6b3db87ddf2a2e9f29ab
Raphi <raphi@localadmins-MacBook-Pro.local>
2018-08-23 11:56:40 +0200

    raw_input

--- Exercise6.py
+++ Exercise6.py
@@ -1,4 +1,17 @@
 # coding=utf-8
 print "exercise6"
 print "change in github"
-print "ich erklaere dir alles"
+
+
+#name = raw_input('What is your name? ')
+
+#print 'Your name is: ', name
+
+a1 = raw_input ('Number1' =)
+a2 = raw_input (Number2 = )
+a3 = raw_input (Number3 = )
+a4 = raw_input (Number4 = )
+
+
+
+print 'a1+a2', a1 + a2
