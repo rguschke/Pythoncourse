47fc37149a6da3949e596aede35737668fc72a5b
Raphi <raphi@localadmins-MacBook-Pro-4.local>
2018-09-12 23:15:32 +0200

    ex14/15

--- Exercise14.py
+++ Exercise14.py
@@ -1,4 +1,21 @@
 from sys import argv
 
-sript, user_name = argv
-prompt = '>'
+script, user_name = argv
+prompt = '> '
+
+print "Hello this is my %s and its called %s"  % (script, user_name)
+print "Id like to ask you a few questions."
+print "Do you like me %s" % user_name
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have %s" % user_name
+computer = raw_input(prompt)
+
+print """
+okay, you said you like me %s.
+You live in %s.
+And you have this computer %s.
+""" % (likes, lives, computer)
